ae82be8b0bf73a82c3403b9f066906334e0ca696
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-09-14 20:08:35 -0300

    Initial commit

---
+++ src/test/java/br/famadev/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.famadev.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
