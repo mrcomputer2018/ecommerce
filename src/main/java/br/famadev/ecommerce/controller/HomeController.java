f368f06a9fba41167f7dff410e5ad8a31f174d1e
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-09-14 20:30:48 -0300

    pagina inicial

--- src/main/java/br/famadev/ecommerce/controller/HomeController.java
+++ src/main/java/br/famadev/ecommerce/controller/HomeController.java
@@ -1,7 +1,15 @@
 package br.famadev.ecommerce.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("HomeController.index()");
+        return "index";
+    }
+
 }
